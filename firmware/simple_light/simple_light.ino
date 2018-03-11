85e0c0020fafe3a96661704ed2b24b42da54e89a
Mike Machado <mike@machadolab.com>
2018-03-10 16:28:39 -0800

    fix signal issues finally

--- firmware/simple_light/simple_light.ino
+++ firmware/simple_light/simple_light.ino
@@ -1,31 +1,38 @@
 #include <EnableInterrupt.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 
 /* rc configuration */
 #define RC_PULSE_CENTER         1500 // pulse width (microseconds) for RC center
 #define PULSE_WIDTH_DEADBAND    100 // pulse width difference from RC_PULSE_CENTER us (microseconds) to ignore (to compensate trim and drift)
-#define RC_PULSE_TIMEOUT        500 // in milliseconds
+#define RC_PULSE_TIMEOUT        1000 // in milliseconds
 
 #define RC_SWITCH_CHANNEL       0
-#define RC_SWITCH_INPUT         3
+#define RC_SWITCH_INPUT         2 // arduino pin connected to R/C receiver
 
 #define RC_NUM_CHANNELS         1
 
 
 /* led strip configuration */
 #define LED_COLOR_ON            Adafruit_NeoPixel::Color(0, 255, 0)
-#define LED_COLOR_OFF           Adafruit_NeoPixel::Color(255, 50, 0)
-#define LED_COLOR_NOSIG         Adafruit_NeoPixel::Color(0, 0, 50)
+#define LED_COLOR_OFF           Adafruit_NeoPixel::Color(0, 0, 0)
+#define LED_COLOR_NO_RC         Adafruit_NeoPixel::Color(255, 0, 0)
 #define LED_COUNT               10
 #define LED_TYPE                NEO_GRB + NEO_KHZ800
 
-#define LED_OUTPUT              4
+#define LED_SHOW_INTERVAL       20
+
+#define LED_OUTPUT              4 // arduino pin with leds
 
 
+enum state_t {
+    STATE_ON,
+    STATE_OFF,
+    STATE_NO_RC
+};
 
 uint16_t rc_values[RC_NUM_CHANNELS];
 uint32_t rc_start[RC_NUM_CHANNELS];
@@ -33,16 +40,26 @@ volatile uint16_t rc_shared[RC_NUM_CHANNELS];
 
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_OUTPUT, LED_TYPE);
 
-long last_rc_pulse = 0;
-int rc_switch_pulse = 0;
+volatile boolean rc_process = false;
+volatile long last_rc_pulse = 0;
+volatile uint16_t rc_switch_pulse = 0;
+state_t state = STATE_OFF;
+long show_time = millis();
 
+
+void disable_rc_processing();
+void enable_rc_processing();
+void setStripColor(uint32_t color);
+void run_state_on();
+void run_state_off();
+void run_state_no_rc();
 
 void rc_read_values() {
   noInterrupts();
   memcpy(rc_values, (const void *)rc_shared, sizeof(rc_shared));
   interrupts();
 
-  rc_switch_pulse = (int)rc_values[RC_SWITCH_CHANNEL];
+  rc_switch_pulse = (uint16_t)rc_values[RC_SWITCH_CHANNEL];
 
   if (millis() - last_rc_pulse > RC_PULSE_TIMEOUT) {
     rc_switch_pulse = 0;
@@ -50,13 +67,21 @@ void rc_read_values() {
 }
 
 void calc_input(uint8_t channel, uint8_t input_pin) {
-  if (digitalRead(input_pin) == HIGH) {
-    rc_start[channel] = micros();
-  } else {
-    uint16_t rc_compare = (uint16_t)(micros() - rc_start[channel]);
-    rc_shared[channel] = rc_compare;
+  if (rc_process) {
+    if (digitalRead(input_pin) == HIGH) {
+      rc_start[channel] = (uint32_t )micros();
+    } else {
+      if (rc_start[channel] > 0) {
+        uint16_t usecs = (uint16_t) (micros() - rc_start[channel]);
+        if (usecs > 900 && usecs < 2100) {
+          rc_shared[channel] = usecs; // only use sane value
+        }
+
+      }
+
+    }
+    last_rc_pulse = millis();
   }
-  last_rc_pulse = millis();
 }
 
 void calc_switch() { calc_input(RC_SWITCH_CHANNEL, RC_SWITCH_INPUT); }
@@ -64,24 +89,79 @@ void calc_switch() { calc_input(RC_SWITCH_CHANNEL, RC_SWITCH_INPUT); }
 void setup() {
   enableInterrupt(RC_SWITCH_INPUT, calc_switch, CHANGE);
 
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+
   leds.begin();
+  
+  state = STATE_OFF;
 }
 
 void loop() {
   rc_read_values();
 
-  if (rc_switch_pulse > RC_PULSE_CENTER + PULSE_WIDTH_DEADBAND) {
-    setStripColor(LED_COLOR_ON);
-  } else if (rc_switch_pulse > 0 && rc_switch_pulse < RC_PULSE_CENTER - PULSE_WIDTH_DEADBAND) {
-    setStripColor(LED_COLOR_OFF);
-  } else {
-    setStripColor(LED_COLOR_NOSIG);
+  if (rc_switch_pulse <= 0) {
+    state = STATE_NO_RC;
   }
-  leds.show();
+
+  switch(state) {
+    case STATE_ON:
+      run_state_on();
+          break;
+    case STATE_OFF:
+      run_state_off();
+          break;
+    case STATE_NO_RC:
+      run_state_no_rc();
+          break;
+  }
+
+  if (show_time + LED_SHOW_INTERVAL < millis()) {
+    disable_rc_processing();
+    leds.show();
+    show_time = millis();
+    enable_rc_processing();
+  }
+}
+
+void disable_rc_processing() {
+  rc_process = false;
+  for (int i = 0; i < RC_NUM_CHANNELS; i++) {
+    rc_start[i] = 0;
+  }
+}
+
+void enable_rc_processing() {
+  rc_process = true;
+}
+
+void run_state_on() {
+  setStripColor(LED_COLOR_ON);
+
+  if (rc_switch_pulse > 0 && rc_switch_pulse < (RC_PULSE_CENTER - PULSE_WIDTH_DEADBAND)) {
+    state = STATE_OFF;
+  }
+}
+
+void run_state_off() {
+  setStripColor(LED_COLOR_OFF);
+
+  if (rc_switch_pulse > (RC_PULSE_CENTER + PULSE_WIDTH_DEADBAND)) {
+    state = STATE_ON;
+  }
+}
+
+void run_state_no_rc() {
+  setStripColor(LED_COLOR_NO_RC);
+
+  if (rc_switch_pulse > 500) {
+    state = STATE_OFF;
+  }
 }
 
 void setStripColor(uint32_t color) {
-  for (int i = 0; i < LED_COUNT; i++) {
+  for (uint16_t i = 0; i < LED_COUNT; i++) {
     leds.setPixelColor(i, color);
   }
 }
--- firmware/turn_signal/turn_signal.ino
+++ firmware/turn_signal/turn_signal.ino
@@ -7,28 +7,31 @@
 
 /* rc configuration */
 #define RC_PULSE_CENTER         1500 // pulse width (microseconds) for RC center
-#define PULSE_WIDTH_DEADBAND    25 // pulse width difference from RC_PULSE_CENTER us (microseconds) to ignore (to compensate trim and drift)
-#define RC_PULSE_TIMEOUT        500 // in milliseconds
-// uncomment to switch left/right signal from RC
-//#define RC_REVERSE
+#define PULSE_WIDTH_DEADBAND    100 // pulse width difference from RC_PULSE_CENTER us (microseconds) to ignore (to compensate trim and drift)
+#define RC_PULSE_TIMEOUT        1000 // in milliseconds
 
 #define RC_TURN_CHANNEL         0
-#define RC_TURN_INPUT           3
+#define RC_TURN_INPUT           2 // arduino pin connected to R/C receiver
 
 #define RC_NUM_CHANNELS         1
 
 
 /* led strip configuration */
-#define LED_COLOR               Adafruit_NeoPixel::Color(0, 255, 0)
-#define LED_COLOR_NO_RC         Adafruit_NeoPixel::Color(0, 0, 50)
-#define LED_ANIM_TIME           200 // in milliseconds
+#define LED_COLOR               Adafruit_NeoPixel::Color(0, 0, 40)
+#define LED_COLOR_NO_RC         Adafruit_NeoPixel::Color(30, 0, 0)
+#define LED_ANIM_TIME           800 // in milliseconds
 #define LED_COUNT               10
 #define LED_TYPE                NEO_GRB + NEO_KHZ800
 
-// uncomment to switch logic from the left side being first to the right side being first
-//#define LED_RIGHT_FIRST
+#define LED_SHOW_INTERVAL       20
 
-#define LED_OUTPUT              4
+                                // swap these values if you need to reverse the lights
+#define LED_LEFT_START          LED_COUNT/2
+#define LED_RIGHT_START         LED_COUNT/2
+#define LED_LEFT_REVERSE
+//#define LED_RIGHT_REVERSE
+
+#define LED_OUTPUT              4 // arduino pin with leds
 
 
 enum state_t {
@@ -44,15 +47,25 @@ volatile uint16_t rc_shared[RC_NUM_CHANNELS];
 
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_OUTPUT, LED_TYPE);
 
-long last_rc_pulse = 0;
+volatile long last_rc_pulse = 0;
 long anim_start = 0;
-uint16_t rc_turn_pulse = 0;
+long debug_time = millis();
+long show_time = millis();
+
+volatile boolean rc_process = false;
+volatile uint16_t rc_turn_pulse = 0;
 state_t state = STATE_IDLE;
 
-void animate_turn_left();
-void animate_turn_right();
+void disable_rc_processing();
+void enable_rc_processing();
 void animate_leds(uint16_t start);
-void animate_no_rc();
+void run_state_idle();
+void run_state_left();
+void run_state_right();
+void run_state_no_rc();
+boolean is_rc_left();
+boolean is_rc_right();
+boolean is_rc_ok();
 
 void rc_read_values() {
     noInterrupts();
@@ -67,19 +80,31 @@ void rc_read_values() {
 }
 
 void calc_input(uint8_t channel, uint8_t input_pin) {
-    if (digitalRead(input_pin) == HIGH) {
-        rc_start[channel] = (uint32_t )micros();
-    } else {
-        uint16_t rc_compare = (uint16_t)(micros() - rc_start[channel]);
-        rc_shared[channel] = rc_compare;
+    if (rc_process) {
+        if (digitalRead(input_pin) == HIGH) {
+            rc_start[channel] = (uint32_t )micros();
+        } else {
+            if (rc_start[channel] > 0) {
+                uint16_t usecs = (uint16_t) (micros() - rc_start[channel]);
+                if (usecs > 900 && usecs < 2100) {
+                    rc_shared[channel] = usecs; // only use sane value
+                }
+            }
+        }
+        last_rc_pulse = millis();
     }
-    last_rc_pulse = millis();
 }
 
 void calc_turn() { calc_input(RC_TURN_CHANNEL, RC_TURN_INPUT); }
 
 void setup() {
+//    Serial.begin(9600);
+
     enableInterrupt(RC_TURN_INPUT, calc_turn, CHANGE);
+
+#if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
 
     leds.begin();
 }
@@ -87,89 +112,141 @@ void setup() {
 void loop() {
     rc_read_values();
 
-    // turn right
-    if (rc_turn_pulse > RC_PULSE_CENTER + PULSE_WIDTH_DEADBAND) {
-#ifdef RC_REVERSE
-        animateTurnLeft();
-        state = STATE_LEFT;
-#else
-        animate_turn_right();
-        state = STATE_RIGHT;
-#endif
+//    if (debug_time + 1000 < millis()) {
+//        Serial.print("r/c: ");
+//        Serial.print(rc_turn_pulse);
+//        Serial.println("");
+//        debug_time = millis();
+//    }
 
-    // turn left
-    } else if (rc_turn_pulse > 0 && rc_turn_pulse < RC_PULSE_CENTER - PULSE_WIDTH_DEADBAND) {
-#ifdef RC_REVERSE
-        animateTurnRight();
-        state = STATE_RIGHT;
-#else
-        animate_turn_left();
-        state = STATE_LEFT;
-#endif
-
-    // no r/c signal
-    } else if (rc_turn_pulse <= 0) {
+    if (!is_rc_ok()) {
         state = STATE_NO_RC;
-        animate_no_rc();
+    }
 
+    switch(state) {
+        case STATE_IDLE:
+            run_state_idle();
+            break;
+        case STATE_RIGHT:
+            run_state_right();
+            break;
+        case STATE_LEFT:
+            run_state_left();
+            break;
+        case STATE_NO_RC:
+            run_state_no_rc();
+            break;
+    }
+
+    if (show_time + LED_SHOW_INTERVAL < millis()) {
+        disable_rc_processing();
+        leds.show();
+        show_time = millis();
+        enable_rc_processing();
+    }
+
+}
+
+void disable_rc_processing() {
+    rc_process = false;
+    for (int i = 0; i < RC_NUM_CHANNELS; i++) {
+        rc_start[i] = 0;
+    }
+}
+
+void enable_rc_processing() {
+    rc_process = true;
+}
+
+
+void run_state_idle() {
+    leds.clear();
+    anim_start = 0;
+
+    if (is_rc_left()) {
+        state = STATE_LEFT;
+    } else if (is_rc_right()) {
+        state = STATE_RIGHT;
+    }
+}
+
+void run_state_left() {
+#ifdef LED_LEFT_REVERSE
+  animate_leds(LED_LEFT_START, true); 
+#else
+  animate_leds(LED_LEFT_START, false);
+#endif
+    
+
+    if (is_rc_right()) {
+        state = STATE_RIGHT;
+    } else if (is_rc_left()) {
+        state = STATE_LEFT;
     } else {
         state = STATE_IDLE;
-        anim_start = 0;
-        leds.clear();
-    }
-    leds.show();
-}
-
-void animate_no_rc() {
-    static boolean leds_on = false;
-
-    if (anim_start > 0) {
-        if (anim_start + LED_ANIM_TIME > millis()) {
-            anim_start = 0;
-            if (leds_on) {
-                leds.clear();
-            } else {
-                for (uint16_t i = 0; i < LED_COUNT; i++) {
-                    leds.setPixelColor(i, LED_COLOR_NO_RC);
-                }
-            }
-            leds_on = !leds_on;
-        }
-    } else {
-        anim_start = millis();
     }
 }
 
-void animate_turn_right() {
-#ifdef LED_RIGHT_FIRST
-    uint16_t start = 0;
+void run_state_right() {
+#ifdef LED_RIGHT_REVERSE
+  animate_leds(LED_RIGHT_START, true);
 #else
-    uint16_t start = LED_COUNT / 2;
+  animate_leds(LED_RIGHT_START, false);
 #endif
-    animate_leds(start);
-}
 
-void animate_turn_left() {
-#ifdef LED_RIGHT_FIRST
-    uint16_t start = LED_COUNT / 2;
-#else
-    uint16_t start = 0;
-#endif
-    animate_leds(start);
-}
-
-void animate_leds(uint16_t start) {
-    if (anim_start > 0) {
-        if (anim_start + LED_ANIM_TIME > millis()) {
-            anim_start = 0;
-            leds.clear();
-        } else {
-            uint16_t led_count = (uint16_t)map(millis() - anim_start, anim_start, millis(), 0, LED_COUNT/2);
-            for (uint16_t i = 0; i < led_count; i++) {
-                leds.setPixelColor(start + i, LED_COLOR);
-            }
-        }
+    if (is_rc_right()) {
+        state = STATE_RIGHT;
+    } else if (is_rc_left()) {
+        state = STATE_LEFT;
     } else {
-        anim_start = millis();
+        state = STATE_IDLE;
     }
 }
+
+void run_state_no_rc() {
+    for (uint16_t i = 0; i < LED_COUNT; i++) {
+        leds.setPixelColor(i, LED_COLOR_NO_RC);
+    }
+
+    if (is_rc_ok()) {
+        state = STATE_IDLE;
+    }
+}
+
+boolean is_rc_left() {
+    return rc_turn_pulse > 0 && rc_turn_pulse < (RC_PULSE_CENTER - PULSE_WIDTH_DEADBAND);
+}
+
+boolean is_rc_right() {
+    return rc_turn_pulse > (RC_PULSE_CENTER + PULSE_WIDTH_DEADBAND);
+}
+
+boolean is_rc_ok() {
+    return rc_turn_pulse > 0;
+}
+
+
+void animate_leds(uint16_t start, boolean reverse) {
+
+    leds.clear();
+
+    long now = millis();
+
+    if (anim_start + LED_ANIM_TIME < now) {
+        anim_start = now;
+    }
+
+    uint16_t led_count = (uint16_t)map(now - anim_start, 0, LED_ANIM_TIME, 1, LED_COUNT/2 + 1);
+    
+    if (reverse) {
+      for (uint16_t i = start; i > start - led_count; i--) {
+        leds.setPixelColor(i-1, LED_COLOR);
+      }
+    } else {
+      for (uint16_t i = 0; i < led_count; i++) {
+        leds.setPixelColor(start + i, LED_COLOR);
+      }
+    }
+    
+}
+
